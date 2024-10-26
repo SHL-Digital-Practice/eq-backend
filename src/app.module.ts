be963c3a6971c25764c6bb89669027214e905e6b
Victor Wanderley Barbosa <vwb@shl.dk>
2024-10-26 22:33:35 +0200

    feat: config things

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,24 @@ import { AppService } from './app.service';
 import { ElementsModule } from './elements/elements.module';
 import { DrizzlePostgresModule } from '@knaadh/nestjs-drizzle-postgres';
 import * as schema from './database/schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ElementsModule,
+    ConfigModule.forRoot(),
     DrizzlePostgresModule.registerAsync({
       tag: 'DB_EQ',
-      useFactory() {
+      useFactory(configService: ConfigService) {
         return {
           postgres: {
-            url: 'postgresql://admin:notsecure@localhost:5432/postgres',
+            url: configService.getOrThrow<string>('DATABASE_URL'),
           },
           config: { schema: { ...schema } },
         };
       },
+      imports: [ConfigModule],
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
